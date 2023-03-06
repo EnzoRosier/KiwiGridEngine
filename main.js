b430c54de6f575da917cb0d7dd1c4323edab5078
DokZokiu <enzo.rosier@student.junia.com>
2023-03-06 22:00:54 +0100

    Doc

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import {GameObject} from './modules/GameObjectClass.js';
+import { GameObject } from "./modules/GameObjectClass.js";
 
 let allObjects = [];
 
-allObjects.push(new GameObject("test"));
+allObjects.push(new GameObject("Bruh", [12, 4]));
 
 console.log(allObjects[0].getObjectName());
--- modules/GameObjectClass.js
+++ modules/GameObjectClass.js
@@ -1,11 +1,23 @@
+/**
+ *
+ *
+ * @class GameObject : Objet de base du jeu
+ */
 class GameObject {
-    constructor(name) {
+    /**
+     * Creates an instance of GameObject.
+     * @param {string} name nom de l'objet
+     * @param {[int x, int y]} pos position x puis y de l'objet
+     * @memberof GameObject
+     */
+    constructor(name, pos) {
         this.name = name;
+        this.posX = pos[0];
+        this.posY = pos[1];
     }
-
     getObjectName() {
-      return this.name;
+        return this.name;
     }
 }
 
-export {GameObject};
+export { GameObject };
