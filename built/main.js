30113247e28fa87945dc0f50d5c78aedfe2bb96c
EnzoRosier <90193056+EnzoRosier@users.noreply.github.com>
2023-03-13 15:45:45 +0100

    a

--- built/main.js
+++ built/main.js
@@ -4,15 +4,15 @@ let allObjects = [];
 let spriteA = new Sprite("./../ressources/Test_SimpleSprite.png", [16, 16]);
 let spriteB = new SpriteAnimation("./../ressources/Test_SpriteSheet.png", [16, 16], [16, 16], 7);
 let objA = new GameObject("objet A", [10, 10]);
-//let objB = new GameObject("objet B", [26, 10]);
+let objB = new GameObject("objet B", [26, 10]);
 objA.addToSpriteCollection("spriteAnim", spriteB);
 objA.addToSpriteCollection("spriteImage", spriteA);
 objA.setCurrentSprite("spriteImage");
-/*objB.addToSpriteCollection("spriteAnim", spriteB);
+objB.addToSpriteCollection("spriteAnim", spriteB);
 objB.addToSpriteCollection("spriteImage", spriteA);
-objB.setCurrentSprite("spriteImage");*/
+objB.setCurrentSprite("spriteAnim");
 allObjects.push(objA);
-//allObjects.push(objB);
+allObjects.push(objB);
 console.log(allObjects[0].getObjectName());
 refreshScreen(allObjects);
 var onoff = false;
@@ -20,13 +20,13 @@ window.onkeydown = function () {
     if (onoff) {
         onoff = false;
         objA.setCurrentSprite("spriteAnim");
-        //objB.setCurrentSprite("spriteAnim");
+        objB.setCurrentSprite("spriteImage");
         refreshScreen(allObjects);
     }
     else {
         onoff = true;
         objA.setCurrentSprite("spriteImage");
-        //objB.setCurrentSprite("spriteImage");
+        objB.setCurrentSprite("spriteAnim");
         refreshScreen(allObjects);
     }
 };
